00ab29564efa75f7a729f3790b291f56051d1510
Administrator <Administrator@PC239>
2020-06-11 11:33:45 +0700

    trang 31

--- BigSchool/BigSchool/Controllers/HomeController.cs
+++ BigSchool/BigSchool/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BigSchool.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +10,20 @@ namespace BigSchool.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+
+        }
+        
         public ActionResult Index()
         {
-            return View();
+            var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)  
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcommingCourses);
         }
 
         public ActionResult About()
